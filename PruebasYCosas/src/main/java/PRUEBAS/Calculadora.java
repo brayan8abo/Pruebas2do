a98787a496c3a19a827cd1a1e1b4401dd5c03178
Brayan8abo <brayan8abo@outlook.es>
2024-10-01 18:22:44 +0200

    update

--- PruebasYCosas/src/main/java/PRUEBAS/Calculadora.java
+++ PruebasYCosas/src/main/java/PRUEBAS/Calculadora.java
@@ -4,13 +4,10 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import java.awt.FlowLayout;
-import javax.swing.border.BevelBorder;
 import java.awt.Color;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.SwingConstants;
-import javax.swing.JTextPane;
 import javax.swing.JButton;
 import javax.swing.border.LineBorder;
 import java.awt.Font;
@@ -48,7 +45,7 @@ public class Calculadora extends JFrame {
 		setBackground(new Color(154, 186, 218));
 		setTitle("CALCULADORA");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 453, 341);
+		setBounds(100, 100, 493, 384);
 
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -67,14 +64,14 @@ public class Calculadora extends JFrame {
 				lblSuma.setText("La suma es: " + (num1 + num2));
 			}
 		});
-		btnSuma.setBounds(32, 35, 89, 23);
+		btnSuma.setBounds(178, 20, 89, 23);
 		contentPane.add(btnSuma);
 		
 		lblSuma = new JLabel("");
 		lblSuma.setFont(new Font("Rubik", Font.PLAIN, 16));
 		lblSuma.setHorizontalAlignment(SwingConstants.CENTER);
 		lblSuma.setBorder(new LineBorder(new Color(0, 0, 0)));
-		lblSuma.setBounds(197, 35, 216, 23);
+		lblSuma.setBounds(118, 54, 216, 23);
 		contentPane.add(lblSuma);
 		
 		JButton btnResta = new JButton("RESTA");
@@ -92,14 +89,14 @@ public class Calculadora extends JFrame {
 				
 			}
 		});
-		btnResta.setBounds(32, 82, 89, 23);
+		btnResta.setBounds(178, 88, 89, 23);
 		contentPane.add(btnResta);
 		
 		lblResta = new JLabel("");
 		lblResta.setHorizontalAlignment(SwingConstants.CENTER);
 		lblResta.setFont(new Font("Rubik", Font.PLAIN, 16));
 		lblResta.setBorder(new LineBorder(new Color(0, 0, 0)));
-		lblResta.setBounds(197, 82, 216, 23);
+		lblResta.setBounds(118, 122, 216, 23);
 		contentPane.add(lblResta);
 		
 		JButton btnMultiplicacion = new JButton("MULTIPLICACIÓN");
@@ -116,14 +113,14 @@ public class Calculadora extends JFrame {
 				
 			}
 		});
-		btnMultiplicacion.setBounds(32, 137, 135, 21);
+		btnMultiplicacion.setBounds(159, 156, 135, 21);
 		contentPane.add(btnMultiplicacion);
 		
 		lblMultiplicacion = new JLabel("");
 		lblMultiplicacion.setHorizontalAlignment(SwingConstants.CENTER);
 		lblMultiplicacion.setFont(new Font("Rubik", Font.PLAIN, 16));
 		lblMultiplicacion.setBorder(new LineBorder(new Color(0, 0, 0)));
-		lblMultiplicacion.setBounds(197, 137, 216, 23);
+		lblMultiplicacion.setBounds(107, 188, 216, 23);
 		contentPane.add(lblMultiplicacion);
 		
 		JButton btnDivision = new JButton("DIVISIÓN");
@@ -141,14 +138,14 @@ public class Calculadora extends JFrame {
 
 			}
 		});
-		btnDivision.setBounds(32, 197, 89, 23);
+		btnDivision.setBounds(178, 222, 89, 23);
 		contentPane.add(btnDivision);
 		
 		lblDivision = new JLabel("");
 		lblDivision.setHorizontalAlignment(SwingConstants.CENTER);
 		lblDivision.setFont(new Font("Rubik", Font.PLAIN, 16));
 		lblDivision.setBorder(new LineBorder(new Color(0, 0, 0)));
-		lblDivision.setBounds(197, 197, 216, 23);
+		lblDivision.setBounds(118, 267, 216, 23);
 		contentPane.add(lblDivision);
 		
 		JButton btnRESET = new JButton("RESET");
@@ -160,10 +157,12 @@ public class Calculadora extends JFrame {
 				lblDivision.setText("");
 			}
 		});
-		btnRESET.setBounds(159, 268, 89, 23);
+		btnRESET.setBounds(178, 311, 89, 23);
 		contentPane.add(btnRESET);
 		
 		
 		
 	}
 }
+// te digo que el boton este arriba y la respuesta abajo
+//asi? si esta ,mejor
